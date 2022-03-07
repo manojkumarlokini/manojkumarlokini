4aeca809243dc0069e7bfa7e8051256ba391c505
manojkumarlokini <100200260+manojkumarlokini@users.noreply.github.com>
2022-03-07 14:52:34 +0530

    Create Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,12 @@
+// Write a program to print your Name, Father's Name and Mailing Address in different lines
+
+class Exercise1 {
+public static void main(String agrs[])
+{
+  System.out.println("my name is Manoj");
+  System.out.println("my father name is Ramu");
+  System.out.println("my mailing address is manojkumarlokini@gmail.com")
+  }
+
+
+} 
